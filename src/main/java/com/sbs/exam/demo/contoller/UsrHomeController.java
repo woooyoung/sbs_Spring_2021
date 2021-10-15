b89556f1df69038db63dd071a5cc24533cff253c
woooyoung <axdsw121@gmail.com>
2021-10-15 17:43:19 +0900

    호출시 반환값이 1씩 증가하는 컨트롤러 구현

--- src/main/java/com/sbs/exam/demo/contoller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/contoller/UsrHomeController.java
@@ -6,20 +6,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int count;
+
+	UsrHomeController() {
+		count = -1;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
 	}
+
 	@RequestMapping("/usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "반갑습니다";
 	}
+
 	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
-		return "잘가세요";
+		return "잘가세요"; 
+	}
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
 	}
 }
-
