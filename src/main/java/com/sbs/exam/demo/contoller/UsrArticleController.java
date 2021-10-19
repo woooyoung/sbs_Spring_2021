7a900cbcd5b2ec3e8130fcd20a26d83eef1ece74
woooyoung <axdsw121@gmail.com>
2021-10-19 16:46:54 +0900

    게시물 테스트데이터 10개 자동생성

--- src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
@@ -11,21 +11,37 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	private int articleLastId;
+	private int articlesLastId;
 	private List<Article> articles;
 
 	public UsrArticleController() {
+		articlesLastId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1; 
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+
+			articles.add(article);
+			articlesLastId = id;
+		}
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articleLastId + 1;
+		int id = articlesLastId + 1; 
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
-		articleLastId = id;
+		articlesLastId = id;
 
 		return article;
 	}
@@ -34,5 +50,12 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		return id + "번 게시물을 삭제했습니다.";
 	}
 }
