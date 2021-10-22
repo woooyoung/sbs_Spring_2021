f51bfdd807550766fff00d391d3e0e04e6f493a0
woooyoung <axdsw121@gmail.com>
2021-10-22 17:39:34 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.sbs.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.sbs.exam.demo.vo.Article;
 
@@ -18,13 +20,16 @@ public interface ArticleRepository {
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	// DELETE * FROM article WHERE id = ?
-	public void deleteArticle(int id);
+	// DELETE FROM article WHERE id = ?
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 
-	// UPDATE article SET regDate = NOW(), updateDate = NOW(), title = ? , body = ?
+	// UPDATE article SET updateDate = NOW(), title = ? , body = ?
 	// WHERE id = ?
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE  article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
 	// SELECT * FROM article ORDER BY id DESC
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 }
