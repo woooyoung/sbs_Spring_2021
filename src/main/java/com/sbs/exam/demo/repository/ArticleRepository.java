373a4b8a54fc596a71fd2c8f87ef13fc5e3972ef
woooyoung <axdsw121@gmail.com>
2021-10-22 17:00:59 +0900

    마이바티스, MySql JDBC 드라이버 추가, 설정파일(yml)에 DB접속정보 입력

--- src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
@@ -1,64 +1,30 @@
 package com.sbs.exam.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.sbs.exam.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private List<Article> articles;
-	private int articlesLastId;
+@Mapper
+public interface ArticleRepository {
 
-	public ArticleRepository() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-	}
+	// INSER INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, body =
+	// ?
+	public Article writeArticle(String title, String body);
 
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticle(@Param("id") int id);
 
-			writeArticle(title, body);
-		}
-	}
+	// DELETE * FROM article WHERE id = ?
+	public void deleteArticle(int id);
 
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
+	// UPDATE article SET regDate = NOW(), updateDate = NOW(), title = ? , body = ?
+	// WHERE id = ?
+	public void modifyArticle(int id, String title, String body);
 
-		articles.add(article);
-		articlesLastId = id;
-
-		return article;
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
+	// SELECT * FROM article ORDER BY id DESC
+	public List<Article> getArticles();
 }
