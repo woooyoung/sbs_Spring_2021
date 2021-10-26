3c63eac1c514cf2a215d6c28fac27b97a2385f5b
woooyoung <axdsw121@gmail.com>
2021-10-26 17:41:51 +0900

    회원가입(doJoin)을 위해 UsrMemberController, MemberService 추가

---
+++ src/main/java/com/sbs/exam/demo/contoller/UsrMemberController.java
@@ -0,0 +1,27 @@
+package com.sbs.exam.demo.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.exam.demo.service.MemberService;
+
+@Controller
+public class UsrMemberController {
+
+	private MemberService memberService;
+
+	public UsrMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
+		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		return "성공";
+	}
+
+}
---
+++ src/main/java/com/sbs/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,5 @@
+package com.sbs.exam.demo.repository;
+
+public class MemberRepository {
+
+}
