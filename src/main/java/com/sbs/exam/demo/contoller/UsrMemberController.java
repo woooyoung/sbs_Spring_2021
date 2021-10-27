7f0ee064dc07770deac01f6f0c8b562b7f866971
woooyoung <axdsw121@gmail.com>
2021-10-27 17:37:30 +0900

    회원가입(DB에 등록) 완료 후 등록된 회원정보 보여줌

--- src/main/java/com/sbs/exam/demo/contoller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/contoller/UsrMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.exam.demo.service.MemberService;
+import com.sbs.exam.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
@@ -17,11 +18,13 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
-		return "성공";
+		Member member = memberService.getMemberById(id);
+
+		return member;
 	}
 
 }
