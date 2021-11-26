39efa124b117f23ea88f96c734d72b8c8088fe64
woooyoung <axdsw121@gmail.com>
2021-11-26 17:34:28 +0900

    리스트에서 검색타입과 검색어에 따라서 총 게시물 수를 나타냄

--- src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
@@ -61,7 +61,8 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/list")
 	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId,
-			@RequestParam(defaultValue = "1") int page) {
+			@RequestParam(defaultValue = "title,body") String searchKeywordTypeCode,
+			@RequestParam(defaultValue = "") String searchKeyword, @RequestParam(defaultValue = "1") int page) {
 
 		Board board = boardService.getBoardById(boardId);
 
@@ -69,16 +70,10 @@ public class UsrArticleController {
 			return rq.historyBackOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
 		}
 
-		int articlesCount = articleService.getArticlesCount(boardId);
+		int articlesCount = articleService.getArticlesCount(boardId, searchKeywordTypeCode, searchKeyword);
 
 		int itemsCountInApage = 10;
 		int pagesCount = (int) Math.ceil((double) articlesCount / itemsCountInApage);
-
-		// 글 20개
-		// 10 * 2
-
-		// 글 21개
-		// 3
 
 		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, itemsCountInApage,
 				page);
