f84423dc5e6d03bd5f8c04b6f0da7b4b7d4f3e39
woooyoung <axdsw121@gmail.com>
2021-11-29 17:51:48 +0900

    조회수 증가를 다른 액션에서 수행하도록

--- src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/contoller/UsrArticleController.java
@@ -91,18 +91,24 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(Model model, int id) {
 
-		ResultData increaseHitCountRd = articleService.increaseHitCount(id);
-//		System.out.println(increaseHitCountRd);
-
-		if (increaseHitCountRd.isFail()) {
-			return rq.historyBackOnView(increaseHitCountRd.getMsg());
-		}
-
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		model.addAttribute("article", article);
 
 		return "usr/article/detail";
+	}
+
+	@RequestMapping("usr/article/doincreaseHitCount")
+	@ResponseBody
+	public ResultData<Integer> doincreaseHitCount(int id) {
+
+		ResultData increaseHitCountRd = articleService.increaseHitCount(id);
+
+		if (increaseHitCountRd.isFail()) {
+			return increaseHitCountRd;
+		}
+
+		return ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
 	}
 
 	@RequestMapping("/usr/article/getArticle")
