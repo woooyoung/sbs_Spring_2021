c730e63754112c5c76beadd8cf9b63ea82c95fac
woooyoung <axdsw121@gmail.com>
2021-12-02 18:01:07 +0900

    리액션 포인트 서비스,리포지터리 도입 중

--- src/main/java/com/sbs/exam/demo/service/ArticleService.java
+++ src/main/java/com/sbs/exam/demo/service/ArticleService.java
@@ -120,8 +120,4 @@ public class ArticleService {
 		return articleRepository.getArticleHitCount(id);
 	}
 
-	public boolean actorCanMakeReactionPoint(int actorId, int id) {
-		return articleRepository.getSumReactionPoint(id, actorId) == 0;
-	}
-
 }
